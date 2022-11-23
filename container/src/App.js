9ce6b25fb607f40f67ac0d8f94b36a7c16f5a6e5
Varun Kumar <kumarvarun2301@gmail.com>
2022-11-23 16:11:31 +0530

    first commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => (
+    <>
+        <h1>Hi there</h1>
+        <hr />
+        <MarketingApp />
+    </>
+)
