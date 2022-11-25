0710499c2592fdcbf84f69409457c757e2c40b31
Varun Kumar <varun.kumar@tavant.com>
2022-11-25 11:37:06 +0530

    css handling

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container',
+})
+
 export default () => (
-    <>
-        <h1>Hi there!!</h1>
-        <hr />
-        <MarketingApp />
-    </>
+    <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName} >
+            <Header />
+            <MarketingApp />
+        </StylesProvider>
+    </BrowserRouter>
 )
---
+++ container/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <>
+        <h1>Hi there from Header!!</h1>
+        <hr />
+    </>
+)
