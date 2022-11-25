326785805a1f21b2233baabd5ebb7d2c54056101
Varun Kumar <varun.kumar@tavant.com>
2022-11-25 10:54:36 +0530

    update marketing deployment

--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -69,81 +69,81 @@ export default function Album() {
     return (
         <React.Fragment>
             <main>
-              {/* Hero unit */}
-              <div className={classes.heroContent}>
-                <Container maxWidth="sm">
-                  <Typography
-                    component="h1"
-                    variant="h2"
-                    align="center"
-                    color="textPrimary"
-                    gutterBottom
-                  >
-                    Home Page
-                  </Typography>
-                  <Typography
-                    variant="h5"
-                    align="center"
-                    color="textSecondary"
-                    paragraph
-                  >
-                    Something short and leading about the collection below—its
-                    contents, the creator, etc. Make it short and sweet, but not too
-                    short so folks don&apos;t simply skip over it entirely.
-                  </Typography>
-                  <div className={classes.heroButtons}>
-                    <Grid container spacing={2} justifyContent="center">
-                      <Grid item>
-                        <Link to="/pricing">
-                          <Button variant="contained" color="primary">
-                            Pricing
-                          </Button>
-                        </Link>
+                {/* Hero unit */}
+                <div className={classes.heroContent}>
+                  <Container maxWidth="sm">
+                    <Typography
+                      component="h1"
+                      variant="h2"
+                      align="center"
+                      color="textPrimary"
+                      gutterBottom
+                    >
+                      Home Page
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      align="center"
+                      color="textSecondary"
+                      paragraph
+                    >
+                      Something short and leading about the collection below—its
+                      contents, the creator, etc. Make it short and sweet, but not too
+                      short so folks don&apos;t simply skip over it entirely.
+                    </Typography>
+                    <div className={classes.heroButtons}>
+                      <Grid container spacing={2} justifyContent="center">
+                        <Grid item>
+                          <Link to="/pricing">
+                            <Button variant="contained" color="primary">
+                              Pricing
+                            </Button>
+                          </Link>
+                        </Grid>
+                        <Grid item>
+                          <Link to="/pricing">
+                            <Button variant="outlined" color="primary">
+                              Pricing
+                            </Button>
+                          </Link>
+                        </Grid>
                       </Grid>
-                      <Grid item>
-                        <Link to="/pricing">
-                          <Button variant="outlined" color="primary">
-                            Pricing
-                          </Button>
-                        </Link>
+                    </div>
+                  </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                  {/* End hero unit */}
+                  <Grid container spacing={4}>
+                    {cards.map((card) => (
+                      <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card className={classes.card}>
+                          <CardMedia
+                            className={classes.cardMedia}
+                            image="https://source.unsplash.com/random"
+                            title="Image title"
+                          />
+                          <CardContent className={classes.cardContent}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                              Heading
+                            </Typography>
+                            <Typography>
+                              This is a media card. You can use this section to describe
+                              the content.
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small" color="primary">
+                              View
+                            </Button>
+                            <Button size="small" color="primary">
+                              Edit
+                            </Button>
+                          </CardActions>
+                        </Card>
                       </Grid>
-                    </Grid>
-                  </div>
+                    ))}
+                  </Grid>
                 </Container>
-              </div>
-              <Container className={classes.cardGrid} maxWidth="md">
-                {/* End hero unit */}
-                <Grid container spacing={4}>
-                  {cards.map((card) => (
-                    <Grid item key={card} xs={12} sm={6} md={4}>
-                      <Card className={classes.card}>
-                        <CardMedia
-                          className={classes.cardMedia}
-                          image="https://source.unsplash.com/random"
-                          title="Image title"
-                        />
-                        <CardContent className={classes.cardContent}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            Heading
-                          </Typography>
-                          <Typography>
-                            This is a media card. You can use this section to describe
-                            the content.
-                          </Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Button size="small" color="primary">
-                            View
-                          </Button>
-                          <Button size="small" color="primary">
-                            Edit
-                          </Button>
-                        </CardActions>
-                      </Card>
-                    </Grid>
-                  ))}
-                </Grid>
-              </Container>
             </main>
             {/* Footer */}
             <footer className={classes.footer}>
